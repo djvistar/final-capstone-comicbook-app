266bb4eeb77ecb143c5d6216510b0bd1b385e775
Blase Pinkert <blaze@blazevo.com>
2021-04-08 14:49:46 -0400

    reworked schema, believe it's operaing correctly, testing for multiple issues in single collection

--- java/database/schema-Blase.sql
+++ java/database/schema-Blase.sql
@@ -1,6 +1,5 @@
 BEGIN TRANSACTION;
 
-
 DROP TABLE IF EXISTS user_collections;
 DROP TABLE IF EXISTS collections;
 DROP TABLE IF EXISTS issue;
@@ -20,7 +19,7 @@ CREATE SEQUENCE seq_user_id
   NO MINVALUE
   CACHE 1;
 
-
+--users created first, not reliant on other tables
 CREATE TABLE users (
 	user_id int DEFAULT nextval('seq_user_id'::regclass) NOT NULL,
 	username varchar(50) NOT NULL,
@@ -28,6 +27,8 @@ CREATE TABLE users (
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
+
+--issues created second, not reliant on other tables
 CREATE TABLE issue (
 issue_id int NOT NULL,
 issue_number int Not Null,
@@ -36,23 +37,26 @@ volume_id int NOT NULL,
 volume_name varchar(200),
 cover_url varchar,
 Constraint PK_issue PRIMARY KEY (issue_id)
- 
 );
-
-CREATE TABLE collections(
-collection_id int DEFAULT nextval('seq_collection_id'::regclass) NOT NULL,
-issue_id int,
-Constraint PK_Collections PRIMARY KEY (collection_id),
-Constraint FK_collections Foreign Key (issue_id) REFERENCES issue(issue_id)     
-);
-
+--user_collections created third, reliant only on USERS
 CREATE TABLE user_collections (
+        collection_pk int NOT NULL,
         collection_id int NOT NULL,
         user_id int NOT NULL,
-        Constraint PK_userCollection PRIMARY KEY (user_id, collection_id),
-        CONSTRAINT FK_user FOREIGN KEY(user_id) REFERENCES users(user_id),
-        CONSTRAINT FK_collectionid FOREIGN KEY (collection_id) references collections(collection_id)  
+        Constraint PK_userCollection PRIMARY KEY (collection_id),
+        CONSTRAINT FK_user FOREIGN KEY(user_id) REFERENCES users(user_id)
+        --Constraint fk_collection Foreign key (collection_id) references collections(collection_id)
 );
+--Collections created 4th reliant on and references both issues and user collections
+CREATE TABLE collections(
+inventory_id int NOT NULL,
+collection_id int NOT NULL,
+issue_id int,
+Constraint PK_invID Primary Key (inventory_id),
+Constraint FK_collectionsissue Foreign Key (issue_id) REFERENCES issue(issue_id),   
+Constraint FK_collectionsid Foreign Key (collection_id)references user_collections(collection_id) 
+);
+
 
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
--- java/database/scratch_queries.sql
+++ java/database/scratch_queries.sql
@@ -1,10 +1,27 @@
 insert into collections (collection_id)
-values(1)
+values(4)
 insert into user_collections(collection_id, user_id) 
-values(1,1);
+values(4,1);
 join users u on uc.user_id = u.user_id
 Where u.user_id =1;
 
+--COLLECTION DAO
+--creates collection
+insert into collections (collection_id)
+values(1)
+
+--adds issue to collection
+update collections set issue_id = 502
+where collection_id = 4
+
+Select * from issue i
+JOIN collection
+where collection_id = 4
+
+--COMIC DAO
+--inserts issue into db
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(503, 12345, 'Fake Comic', 54321, 'fake volume', 'https://picsum.photos/id/237/200/300')
 
 
 
