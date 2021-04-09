85c2f67d735e23cb5e1a9f9e3cf0387ed9aea840
Student <student@Techs-MacBook-Air.local>
2021-04-09 09:35:01 -0400

    push

--- java/src/main/java/com/techelevator/controller/ComicVineAPIController.java
+++ java/src/main/java/com/techelevator/controller/ComicVineAPIController.java
@@ -23,34 +23,34 @@ import com.techelevator.model.Volume;
 @RestController
 public class ComicVineAPIController {
 	
-//	@RequestMapping("/addVolume/{volumeTitle}")
-//	public List<Volume> getVolume(@PathVariable String volumeTitle, HttpServletRequest request) {
-//		
-//		String apiURL = "https://comicvine.gamespot.com/api/";
-//		String volumes = "volumes/?";
-//		String keyValue = "a8be2fbe34bdee38d016c400c675139289302d9d";
-//		String getVolumes = apiURL + volumes + "api_key=" + keyValue + "&format=json&filter=name:" + volumeTitle;
-//		
-//		HttpHeaders httpHeaders = new HttpHeaders();
-//		httpHeaders.add("user-agent", "Mozilla/5.0 Firefox/26.0");
-//		
-//		HttpEntity<String> httpEntity = new HttpEntity<>(httpHeaders);
-//		RestTemplate restTemplate = new RestTemplate();
-//		ObjectMapper objectMapper = new ObjectMapper();
-//		
-//		ResponseEntity<String> response = restTemplate.exchange(getVolumes, HttpMethod.GET, httpEntity, String.class);
-//		
-//		
-//		JsonNode jsonNode = objectMapper.readTree(response.getBody());
-//		List <Volume> volumeList = new ArrayList<Volume>();
-//		
-//		
-//		
-//		
-//		
-//		return volumeList;
-//		
-//		
-//	}
+	@RequestMapping("/addVolume/{volumeTitle}")
+	public List<Volume> getVolume(@PathVariable String volumeTitle, HttpServletRequest request) {
+		
+		String apiURL = "https://comicvine.gamespot.com/api/";
+		String volumes = "volumes/?";
+		String keyValue = "a8be2fbe34bdee38d016c400c675139289302d9d";
+		String getVolumes = apiURL + volumes + "api_key=" + keyValue + "&format=json&filter=name:" + volumeTitle;
+		
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.add("user-agent", "Mozilla/5.0 Firefox/26.0");
+		
+		HttpEntity<String> httpEntity = new HttpEntity<>(httpHeaders);
+		RestTemplate restTemplate = new RestTemplate();
+		ObjectMapper objectMapper = new ObjectMapper();
+		
+		ResponseEntity<String> response = restTemplate.exchange(getVolumes, HttpMethod.GET, httpEntity, String.class);
+		
+		
+		JsonNode jsonNode = objectMapper.readTree(response.getBody());
+		List <Volume> volumeList = new ArrayList<Volume>();
+		
+		
+		
+		
+		
+		return volumeList;
+		
+		
+	}
 
 }
