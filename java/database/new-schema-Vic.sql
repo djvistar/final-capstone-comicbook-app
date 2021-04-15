a5cf18e8762193ffa95fbb083f1d9505e563c850
Student <student@Techs-MacBook-Air.local>
2021-04-15 15:38:53 -0400

    dummy data add

---
+++ java/database/new-schema-Vic.sql
@@ -0,0 +1,189 @@
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(27864, 3, 'Legends from the Darkside', 3816, 'Superman', 'https://comicvine1.cbsistatic.com/uploads/scale_small/11112/111120209/5928963-sm%20%233%20final.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(28085, 5, 'The Mummy Strikes', 3816, 'Superman', 'https://comicvine1.cbsistatic.com/uploads/scale_small/11112/111120209/5928969-sm%20%235%20final.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(153065, 1, 'World of New Krypton Part One', 25900, 'Superman: World of New Krypton', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/9241/726798-00.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(159275, 4, 'World of New Krypton, Part Four', 25900, 'Superman: World of New Krypton', 'https://comicvine1.cbsistatic.com/uploads/scale_small/3/31666/825780-krypton.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(163385, 5, 'World of New Krypton, Part Five', 25900, 'Superman: World of New Krypton', 'https://comicvine1.cbsistatic.com/uploads/scale_small/3/31666/869799-new.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(166116, 6, 'World of New Krypton, Part Six', 25900, 'Superman: World of New Krypton', 'https://comicvine1.cbsistatic.com/uploads/scale_small/3/31666/904520-smwnk_cv6_copy.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(170732, 7, 'World of New Krypton: Part Seven', 25900, 'Superman: World of New Krypton', 'https://comicvine1.cbsistatic.com/uploads/scale_small/2/29350/951304-00.jpg')
+
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(181603, 9, 'World of New Krypton, Part Nine', 25900, 'Superman: World of New Krypton', 'https://comicvine1.cbsistatic.com/uploads/scale_small/3/38919/1017402-superman_world_of_new_krypton_9.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(185625, 10, 'World of New Krypton, Part Ten', 25900, 'Superman: World of New Krypton', 'https://comicvine1.cbsistatic.com/uploads/scale_small/3/31666/1047337-smwnk_cv10_ds_copy.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(21795, 5, 'X-Men: God Loves, Man Kills', 3144, 'Marvel Graphic Novel', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/19524-3144-21795-1-marvel-graphic-novel.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(21788, 2, 'Elric: The Dreaming City', 3144, 'Marvel Graphic Novel', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/19517-3144-21788-1-marvel-graphic-novel.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(21782, 1, 'The Death of Captain Marvel', 3144, 'Marvel Graphic Novel', 'https://comicvine1.cbsistatic.com/uploads/scale_small/11/117763/2716684-marvelgraphicnovel01.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(263252, 22, 'The Amazing Spider-Man: Hooky', 3144, 'Marvel Graphic Novel', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/23610-3144-26325-1-marvel-graphic-novel.jpg')
+
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(117738, 17, 'Revenge of The Living Monolith', 3144, 'Marvel Graphic Novel', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/1714/230211-3144-117738-1-marvel-graphic-novel.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(123224, 18, 'The Sensational She-Hulk', 3144, 'Marvel Graphic Novel', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/77/309925-3144-123224-1-marvel-graphic-novel.jpg')
+
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(123227, 21, 'Marada the She-Wolf', 3144, 'Marvel Graphic Novel', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/77/309928-3144-123227-1-marvel-graphic-novel.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(135287, 38, 'Silver Surfer: Judgment Day', 3144, 'Marvel Graphic Novel', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/77/490721-ssjday.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(743559, 2, null, 126035, 'Web van Spiderman', 'https://comicvine1.cbsistatic.com/uploads/scale_small/11/110017/7295655-img001.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(42970, 1, 'Men of Steel', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/38264-5767-42970-1-superman-adventures.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(43062, 2, 'Be Careful What You Wish For...', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/38339-5767-43062-1-superman-adventures.jpg')
+
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(43258, 3, 'Distant Thunder', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/38499-5767-43258-1-superman-adventures.jpg')
+
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(43359, 4, 'Eye to Eye', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/38588-5767-43359-1-superman-adventures.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(43568, 6, 'Seonimod', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/38765-5767-43568-1-superman-adventures.jpg')
+
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(44068, 11, 'The War Within', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/39185-5767-44068-1-superman-adventures.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(43672, 7, 'All Creatures Great and Small, Part 1', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/38855-5767-43672-1-superman-adventures.jpg')
+
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(44176, 12, 'The War Within, Part Two', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/39274-5767-44176-1-superman-adventures.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(43759, 8, 'All Creatures Great and Small, Part 2', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/38930-5767-43759-1-superman-adventures.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(44284, 13, 'Grand Slam', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/39362-5767-44284-1-superman-adventures.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(43864, 9, 'Return of the Hero', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/39019-5767-43864-1-superman-adventures.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(44384, 14, 'Stop the Presses!', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/39448-5767-44384-1-superman-adventures.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(43963, 10, 'Don't Try This at Home', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/39102-5767-43963-1-superman-adventures.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(44553, 15, 'Maximum Effort!', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/39585-5767-44553-1-superman-adventures.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(44667, 16, 'Clark Kent, You're A Nobody!', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/39688-5767-44667-1-superman-adventures.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(71017, 31, 'Family Reunion: Part Two', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/73827-5767-71017-1-superman-adventures.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(48247, 17, 'Superman's Pal's Pal', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/42342-5767-48247-1-superman-adventures.jpg')
+
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(71018, 32, 'Sullivan's Girl Friend, Lois Lane', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/73828-5767-71018-1-superman-adventures.jpg')
+
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(71019, 33, 'Clark Kent Is Superman - And I Can Prove It!', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/74102-5767-71019-1-superman-adventures.jpg')
+
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(71020, 34, 'Sanctuary', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/74103-5767-71020-1-superman-adventures.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(71021, 35, 'Never Play With the Toyman's Toys!', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/74104-5767-71021-1-superman-adventures.jpg')
+
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(48248, 18, 'It's A Super Life!', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/42343-5767-48248-1-superman-adventures.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(48249, 19, 'The Bodyguard of Steel', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/42344-5767-48249-1-superman-adventures.jpg')
+
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(71037, 56, 'Poker Face', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/53036-5767-71037-1-superman-adventures.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(71023, 40, 'Old Wounds', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/74106-5767-71023-1-superman-adventures.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(71038, 57, '4 Point Perspective', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/53037-5767-71038-1-superman-adventures.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(503, 12345, 'Fake Comic', 54321, 'fake volume', 'https://picsum.photos/id/237/200/300')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(71024, 41, '22 Stories in a Single Bound', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/53023-5767-71024-1-superman-adventures.jpg')
+
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(71039, 58, 'What Lurks Below', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/73829-5767-71039-1-superman-adventures.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(71025, 43, 'Are You My Mother Box?', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/53024-5767-71025-1-superman-adventures.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(71040, 59, 'Ghost Flowers', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/73830-5767-71040-1-superman-adventures.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(71022, 36, 'This Is a Job For Superman', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/74105-5767-71022-1-superman-adventures.jpg')
+
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(71026, 44, 'Law and Orders', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/53025-5767-71026-1-superman-adventures.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(99448, 60, 'Roughnecks', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/66451-5767-99448-1-superman-adventures.jpg')
+
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(99449, 61, 'Nothing Up My Sleeve!', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/66452-5767-99449-1-superman-adventures.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(99454, 66, 'Power Play, Part Two', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/66457-5767-99454-1-superman-adventures.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(99451, 63, 'Magic Wand', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/66454-5767-99451-1-superman-adventures.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(203199, 52, 'A Death in the Family', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/3/36359/1172747-supadv52.jpg')
+
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(99452, 64, 'Brainiac Beyond', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/0/4/66455-5767-99452-1-superman-adventures.jpg')
+
+insert into issue(issue_id, issue_number, issue_name, volume_id, volume_name, cover_url)
+values(234910, 22, 'War Games: Part 1', 5767, 'Superman Adventures', 'https://comicvine1.cbsistatic.com/uploads/scale_small/7/74728/1402746-superman_adventures__22__2010____page_1.jpg')
